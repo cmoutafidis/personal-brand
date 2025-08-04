e04ae91161c9b641277ab4efa7c3547c363bd5f7
cmoutafidis <charismoutafidis@gmail.com>
2025-08-04 23:23:48 +0300

    Nextjs attempt

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -46,7 +46,7 @@ export default function RootLayout({
         <link rel="alternate" hrefLang="x-default" href="https://fijisolutions.net/en"/>
 
         <link rel="preconnect" href="https://fonts.googleapis.com"/>
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin={"true"}/>
+        <link rel="preconnect" href="https://fonts.gstatic.com"/>
         <link
         href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&family=Plus+Jakarta+Sans:wght@500;700&display=swap"
         rel="stylesheet"/>
