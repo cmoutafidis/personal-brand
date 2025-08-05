83d3d47b261d84f72eab672d839b3d3c2d364f40
Fiji <charismoutafidis@gmail.com>
2025-08-05 14:37:16 +0200

    Fix Next.js dynamic import error

--- src/app/[[...slug]]/client.tsx
+++ src/app/[[...slug]]/client.tsx
@@ -1,9 +1,7 @@
 'use client'
 
 import React from 'react'
-import dynamic from 'next/dynamic'
-
-const App = dynamic(() => import('../../App'), { ssr: false })
+import App from '../../App'
 
 export function ClientOnly() {
   return <App />
