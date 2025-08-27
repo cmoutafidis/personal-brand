b07fd3ffe40ee8073b3b69aba7f43c2f716181b4
cmoutafidis <charismoutafidis@gmail.com>
2025-08-27 13:50:12 +0200

    Leadsy

--- next.config.ts
+++ next.config.ts
@@ -8,7 +8,7 @@ const nextConfig: NextConfig = {
         headers: [
           {
             key: 'Content-Security-Policy',
-            value: "script-src 'self' 'unsafe-inline' 'unsafe-eval' blob: https://unpkg.com; object-src 'none';",
+            value: "script-src 'self' 'unsafe-inline' 'unsafe-eval' blob: https://unpkg.com https://r2.leadsy.ai; object-src 'none';",
           },
         ],
       },
