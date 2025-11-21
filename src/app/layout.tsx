4836601a5a760f9d1b58d6f4d63869821c4d2d5c
cmoutafidis <charismoutafidis@gmail.com>
2025-11-21 22:32:54 +0100

    Gtag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -202,6 +202,22 @@ export default function RootLayout({
           })
         }}
       />
+
+      <script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=AW-17750042512"
+      />
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+          
+            gtag('config', 'AW-17750042512');
+          `
+        }}
+      />
     </head>
     <body className="antialiased">
     <ThemeProvider>
