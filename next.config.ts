a872d9ee62b14ba8d9bcab38c3d7d0842b783bc1
cmoutafidis <charismoutafidis@gmail.com>
2025-11-21 22:59:13 +0100

    domains

--- next.config.ts
+++ next.config.ts
@@ -8,7 +8,7 @@ const nextConfig: NextConfig = {
         headers: [
           {
             key: 'Content-Security-Policy',
-            value: "script-src 'self' 'unsafe-inline' 'unsafe-eval' blob: https://unpkg.com https://r2.leadsy.ai https://tag.trovo-tag.com https://www.googletagmanager.com https://googleads.g.doubleclick.net https://www.google.com; connect-src 'self' https://wvbknd.leadsy.ai https://api.vapi.ai https://*.vapi.ai https://*.daily.co wss://*.daily.co https://*.sentry.io https://www.google-analytics.com https://np40nkw6be.execute-api.us-east-1.amazonaws.com https://www.google.com https://googleads.g.doubleclick.net; object-src 'none';",
+            value: "script-src 'self' 'unsafe-inline' 'unsafe-eval' blob: https://unpkg.com https://r2.leadsy.ai https://tag.trovo-tag.com https://www.googletagmanager.com https://googleads.g.doubleclick.net https://www.google.com https://www.googleadservices.com; connect-src 'self' https://wvbknd.leadsy.ai https://api.vapi.ai https://*.vapi.ai https://*.daily.co wss://*.daily.co https://*.sentry.io https://www.google-analytics.com https://np40nkw6be.execute-api.us-east-1.amazonaws.com https://www.google.com https://googleads.g.doubleclick.net https://www.googleadservices.com https://www.google.ch; object-src 'none';",
           },
         ],
       },
